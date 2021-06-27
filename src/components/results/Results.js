c127959239668e736b434ab94e6379027ea9c518
Michael Zimmerman <mikezne@gmail.com>
2021-06-27 13:41:23 -0500

    fixes input problem but no longer has min char.

--- src/components/results/Results.js
+++ src/components/results/Results.js
@@ -9,10 +9,8 @@ const Results = ({ response, search }) => {
   return (
     <div>
       {/* Is searchbox 0? yes, null. no, nested condition */}
-      {searchLength === 0 ? null : searchLength <= 2 ? ( // Is search <= 2? yes, error msg. no nested condition
-        <h1>Your search must have at least 3 characters!</h1>
-      ) : // Are there results? Yes, show results. No, error msg.
-      isResponse === "False" ? (
+
+      {isResponse === "False" ? (
         <h1>No Results! Try new search terms!</h1>
       ) : searchResults === undefined ? null : (
         <h1>Showing 10 of {totalResults} results!</h1> &&
