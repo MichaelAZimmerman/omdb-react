27ca8eaf25720a30dd925809a9d824490881d503
Michael Zimmerman <mikezne@gmail.com>
2021-06-27 20:48:45 -0500

    fixes issues with "Now Showing: # of # results"

--- src/components/results/Results.js
+++ src/components/results/Results.js
@@ -5,14 +5,21 @@ const Results = ({ response }) => {
   const totalResults = response.totalResults;
   const isResponse = response.Response;
   const searchResults = response.Search;
+
   return (
-    <div className="flexBox">
-      {/* Is searchbox 0? yes, null. no, nested condition */}
+    <div className="flexBox justify">
+      {isResponse === "True" ? (
+        <h1>
+          Now Showing: {searchResults.length} of {totalResults} results!
+        </h1>
+      ) : (
+        <h1 />
+      )}
 
       {isResponse === "False" ? (
         <h1>No Results! Try new search terms!</h1>
       ) : searchResults === undefined ? null : (
-        <h1>Showing 10 of {totalResults} results!</h1> &&
+        isResponse === "True" &&
         searchResults.map((response, idx) => (
           <Display key={idx} response={response} />
         ))
